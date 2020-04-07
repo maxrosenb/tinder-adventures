fcde2d14306a2aad3d82220a61dcf6122d3fc016
maxrosenb <maxrosenb@reed.edu>
2020-04-06 17:48:33 -0700

    started project

---
+++ tinder_ai.py
@@ -0,0 +1,102 @@
+import datetime
+import requests
+from random import random
+from time import sleep
+from geopy.geocoders import Nominatim
+
+TINDER_URL = "https://api.gotinder.com"
+geolocator = Nominatim(user_agent="auto-tinder")
+PROF_FILE = "./images/unclassified/profiles.txt"
+
+class Person(object):
+
+    def __init__(self, data, api):
+        self._api = api
+
+        self.id = data["_id"]
+        self.name = data.get("name", "Unknown")
+
+        self.bio = data.get("bio", "")
+        self.distance = data.get("distance_mi", 0) / 1.60934
+
+        self.birth_date = datetime.datetime.strptime(data["birth_date"], '%Y-%m-%dT%H:%M:%S.%fZ') if data.get(
+            "birth_date", False) else None
+        self.gender = ["Male", "Female", "Unknown"][data.get("gender", 2)]
+
+        self.images = list(map(lambda photo: photo["url"], data.get("photos", [])))
+
+        self.jobs = list(
+            map(lambda job: {"title": job.get("title", {}).get("name"), "company": job.get("company", {}).get("name")}, data.get("jobs", [])))
+        self.schools = list(map(lambda school: school["name"], data.get("schools", [])))
+
+        if data.get("pos", False):
+            self.location = geolocator.reverse(f'{data["pos"]["lat"]}, {data["pos"]["lon"]}')
+
+    def download_images(self, folder=".", sleep_max_for=0):
+        with open(PROF_FILE, "r") as f:
+            lines = f.readlines()
+            if self.id in lines:
+                return
+        with open(PROF_FILE, "a") as f:
+            f.write(self.id+"\r\n")
+        index = -1
+        for image_url in self.images:
+            index += 1
+            req = requests.get(image_url, stream=True)
+            if req.status_code == 200:
+                with open(f"{folder}/{self.id}_{self.name}_{index}.jpeg", "wb") as f:
+                    f.write(req.content)
+            sleep(random()*sleep_max_for)
+
+
+    def __repr__(self):
+        return f"{self.id}  -  {self.name} ({self.birth_date.strftime('%d.%m.%Y')})"
+
+
+    def like(self):
+        return self._api.like(self.id)
+
+    def dislike(self):
+        return self._api.dislike(self.id)
+
+
+
+class tinderAPI():
+
+    def __init__(self, token):
+        self._token = token
+
+    def profile(self):
+        data = requests.get(TINDER_URL + "/v2/profile?include=account%2Cuser", headers={"X-Auth-Token": self._token}).json()
+        return Profile(data["data"], self)
+
+    def matches(self, limit=10):
+        data = requests.get(TINDER_URL + f"/v2/matches?count={limit}", headers={"X-Auth-Token": self._token}).json()
+        return list(map(lambda match: Person(match["person"], self), data["data"]["matches"]))
+
+    def like(self, user_id):
+        data = requests.get(TINDER_URL + f"/like/{user_id}", headers={"X-Auth-Token": self._token}).json()
+        return {
+            "is_match": data["match"],
+            "liked_remaining": data["likes_remaining"]
+        }
+
+    def dislike(self, user_id):
+        requests.get(TINDER_URL + f"/pass/{user_id}", headers={"X-Auth-Token": self._token}).json()
+        return True
+
+    def nearby_persons(self):
+        data = requests.get(TINDER_URL + "/v2/recs/core", headers={"X-Auth-Token": self._token}).json()
+        return list(map(lambda user: Person(user["user"], self), data["data"]["results"]))
+
+
+if __name__ == "__main__":
+    token = "a39844e8-2046-4c3c-8a87-8259a488fbdc"
+    api = tinderAPI(token)
+
+    while True:
+        persons = api.nearby_persons()
+        for person in persons:
+            person.download_images(folder="./images/unclassified", sleep_max_for=random()*3)
+            sleep(random()*10)
+        sleep(random()*10)
